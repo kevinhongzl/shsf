c4b0aec70b196305c20151d27c31c939bbc41caa
kevinhongzl <zhenlun.hong01@gmail.com>
2025-07-16 21:33:43 +0800

    chore: finish implementing treemap

--- data-structures/bst/java/src/BSTMap.java
+++ data-structures/bst/java/src/BSTMap.java
@@ -1,13 +1,8 @@
-import java.util.Iterator;
-import java.util.Set;
+import javax.annotation.Nonnull;
+import java.util.*;
 
-public class BSTMap<K extends Comparable<K>, V> extends BST<Tuple<K, V>> implements Map61B<K, V> {
-    public BSTNode<Tuple<K, V>> root;
-
-    public BSTMap() {
-        root = new BSTNode<>();
-        size = 0;
-    }
+public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
+    private BST<Tuple<K, V>> tree = new BST<>();
 
     /** Associates the specified value with the specified key in this map.
      *  If the map already contains the specified key, replaces the key's mapping
@@ -15,46 +10,47 @@ public class BSTMap<K extends Comparable<K>, V> extends BST<Tuple<K, V>> impleme
     @Override
     public void put(K key, V value) {
         Tuple<K, V> target = new Tuple<>(key, value);
-        BSTNode<Tuple<K, V>> result = root.find(target);
-        if (result.item == null) {
-            result.insert(target);
-            size += 1;
-        } else {
-            result.item.value = value;
+        if (tree.contains(target)) {
+            tree.remove(target);
         }
+        tree.put(target);
     }
 
     /** Returns the value to which the specified key is mapped, or null if this
      * map contains no mapping for the key. */
     @Override
     public V get(K key) {
-        BSTNode<Tuple<K, V>> result = root.find(new Tuple<>(key, null));
-        if (result.item == null) {
-            return null;
-        } else {
-            return result.item.value;
-        }
+        Tuple<K, V> result = tree.get(new Tuple<>(key, null));
+        return result != null? result.value : null;
     }
 
     /** Returns whether this map contains a mapping for the specified key. */
     @Override
     public boolean containsKey(K key) {
-        return root.contains(new Tuple<>(key, null));
+        return tree.contains(new Tuple<>(key, null));
+    }
+
+    /** Returns the number of key-value mappings in this map. */
+    @Override
+    public int size() {
+        return tree.size();
     }
 
     /** Removes every mapping from this map. */
     @Override
     public void clear() {
-        root = new BSTNode<>();
-        size = 0;
+        tree = new BST<>();
     }
 
     /** Returns a Set view of the keys contained in this map. Not required for Lab 7.
      * If you don't implement this, throw an UnsupportedOperationException. */
     @Override
     public Set<K> keySet() {
-        // return Set.of();
-        throw new UnsupportedOperationException();
+        Set<K> keySet = new LinkedHashSet<>();
+        for (K key: this) {
+            keySet.add(key);
+        }
+        return keySet;
     }
 
     /** Removes the mapping for the specified key from this map if present,
@@ -64,18 +60,27 @@ public class BSTMap<K extends Comparable<K>, V> extends BST<Tuple<K, V>> impleme
     @Override
     public V remove(K key) {
         Tuple<K, V> target = new Tuple<>(key, null);
-        Tuple<K, V> tupleDeleted = root.delete(target);
-        if (tupleDeleted == null) {
-            return null;
-        } else {
-            size -= 1;
-            return tupleDeleted.value;
-        }
+        Tuple<K, V> tupleDeleted = tree.remove(target);
+        return tupleDeleted != null ? tupleDeleted.value : null;
     }
 
     @Override
+    @Nonnull
     public Iterator<K> iterator() {
-        // return null;
-        throw new UnsupportedOperationException();
+        return new BSTMapIterator();
+    }
+
+    private class BSTMapIterator implements Iterator<K> {
+        private final Iterator<Tuple<K, V>> treeIterator = tree.iterator();
+
+        @Override
+        public boolean hasNext() {
+            return treeIterator.hasNext();
+        }
+
+        @Override
+        public K next() {
+            return treeIterator.next().key;
+        }
     }
 }
