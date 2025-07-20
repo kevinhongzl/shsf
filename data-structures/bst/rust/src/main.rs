d6fd0e26f1eaf37a66aab84cdb9e3a3f4eddc5d2
kevinhongzl <zhenlun.hong01@gmail.com>
2025-07-20 21:33:03 +0800

    feat: implement bst in rust

---
+++ data-structures/bst/rust/src/main.rs
@@ -0,0 +1,55 @@
+#[derive(Debug)]
+struct BSTNode {
+    value: i32,
+    left: Option<Box<BSTNode>>,
+    right: Option<Box<BSTNode>>,
+}
+
+impl BSTNode {
+    fn new(value: i32) -> Self {
+        BSTNode {
+            value: value,
+            left: None,
+            right: None,
+        }
+    }
+
+    fn insert(&mut self, value: i32) {
+        if value <= self.value {
+            if self.left.is_none() {
+                (*self).left = Some(Box::new(BSTNode::new(value)));
+            } else {
+                let node = self.left.as_mut().unwrap();
+                node.insert(value);
+            }
+        } else {
+            if self.right.is_none() {
+                (*self).right = Some(Box::new(BSTNode::new(value)));
+            } else {
+                let node = self.right.as_mut().unwrap();
+                node.insert(value);
+            }
+        }
+    }
+
+    fn preorder(&self) {
+        print!("{} ", self.value);
+        self.left.as_ref().map(|node| {
+            node.preorder();
+        });
+        self.right.as_ref().map(|node| {
+            node.preorder();
+        });
+    }
+}
+
+fn main() {
+    let mut node = BSTNode::new(4);
+    node.insert(2);
+    node.insert(1);
+    node.insert(3);
+    node.insert(5);
+    node.insert(6);
+    node.insert(7);
+    node.preorder();
+}
