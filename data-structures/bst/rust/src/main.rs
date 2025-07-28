ecd9b52977b22d55018d221dcc3f9765d5f9076e
kevinhongzl <zhenlun.hong01@gmail.com>
2025-07-28 21:17:53 +0800

    chore: update rust bst implementation

--- data-structures/bst/rust/src/main.rs
+++ data-structures/bst/rust/src/main.rs
@@ -1,71 +1,71 @@
-use std::cmp::Ordering;
-
 #[derive(Debug)]
 struct BSTNode {
-    value: i32,
+    key: i32,
     left: Option<Box<BSTNode>>,
     right: Option<Box<BSTNode>>,
 }
 
 impl BSTNode {
-    fn new(value: i32) -> Self {
+    fn new(key: i32) -> Self {
         BSTNode {
-            value: value,
+            key: key,
             left: None,
             right: None,
         }
     }
 
-    fn insert(&mut self, value: i32) {
-        let branch = if value <= self.value {
-            &mut self.left
-        } else {
-            &mut self.right
-        };
-        match branch {
-            None => {
-                *branch = Some(Box::new(BSTNode::new(value)));
+    fn insert(node: Option<Box<BSTNode>>, key: i32) -> Option<Box<BSTNode>> {
+        match node {
+            None => Some(Box::new(BSTNode::new(key))),
+            Some(mut n) if key <= n.key => {
+                n.left = BSTNode::insert(n.left, key);
+                Some(n)
             }
-            Some(node) => {
-                node.insert(value);
+            Some(mut n) => {
+                n.right = BSTNode::insert(n.right, key);
+                Some(n)
             }
         }
     }
 
-    fn find(&self, value: i32) -> bool {
-        match value.cmp(&self.value) {
-            Ordering::Equal => true,
-            Ordering::Less => self
-                .left
-                .as_ref()
-                .map_or(false, |branch| branch.find(value)),
-            Ordering::Greater => self
-                .right
-                .as_ref()
-                .map_or(false, |branch| branch.find(value)),
+    fn find(node: &Option<Box<BSTNode>>, key: i32) -> bool {
+        match node {
+            None => false,
+            Some(n) if n.key == key => true,
+            Some(n) if n.key > key => BSTNode::find(&n.left, key),
+            Some(n) => BSTNode::find(&n.right, key),
+        }
+    }
+}
+
+struct BST {
+    root: Option<Box<BSTNode>>,
+    size: i32,
+}
+
+impl BST {
+    fn new() -> Self {
+        BST {
+            root: None,
+            size: 0,
         }
     }
 
-    fn preorder(&self) {
-        print!("{} ", self.value);
-        self.left.as_ref().map(|node| {
-            node.preorder();
-        });
-        self.right.as_ref().map(|node| {
-            node.preorder();
-        });
+    fn insert(&mut self, key: i32) {
+        self.root = BSTNode::insert(self.root.take(), key);
+        self.size += 1;
+    }
+
+    fn find(&self, key: i32) -> bool {
+        BSTNode::find(&self.root, key)
     }
 }
 
 fn main() {
-    let mut tree = BSTNode::new(4);
-    tree.insert(2);
+    let mut tree = BST::new();
     tree.insert(1);
-    tree.insert(3);
-    tree.insert(5);
-    tree.insert(6);
-    tree.insert(7);
-    tree.preorder();
-    println!("\nTree contains 1 : {}", tree.find(1));
-    println!("Tree contains 0 : {}", tree.find(0));
+    tree.insert(0);
+    tree.insert(2);
+    println!("{:?}", tree.root);
+    println!("{:?}", tree.find(12));
 }
